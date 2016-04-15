aa71faeb3d63fa8085a8e22625628fbff03e68de
Kyle McCarthy <km@kylemccarthy.io>
2016-04-15 18:26:51 -0500

    extract the top 10 names in the top 10 states by median income per year

--- income_names.py
+++ income_names.py
@@ -3,7 +3,7 @@ import pandas as pd
 income = pd.read_csv('data/TopMedianIncomeByYear.csv')
 names = pd.read_csv('data/StateNames.csv')
 
-top_names = []
+top_names = pd.DataFrame(columns=names.columns)
 
 # we are essentially going to iterate over the top income states by year, for each state in the year we are then
 # going to look at the most popular names and export a segment of them
@@ -14,6 +14,7 @@ for index, row in income.iterrows():
         # select the subset of names for the year and state and get the most popular ones
         popular = names[(names.Year == year) & (names.State == state)]
         popular = popular.sort_values('Count', ascending=False)
-        print(popular)
-        break
-    break
+        top_names = top_names.append(popular.head(10))
+
+top_names = top_names.drop('Id', axis=1)
+top_names.to_csv('data/TopNamesIncome.csv', index=False)
