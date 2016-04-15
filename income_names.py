6e284594e84eb78cbd80007edfe6e4e96bc9745b
Kyle McCarthy <km@kylemccarthy.io>
2016-04-15 17:58:46 -0500

    wip - aggregating the popular names by state and year

---
+++ income_names.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+income = pd.read_csv('data/TopMedianIncomeByYear.csv')
+names = pd.read_csv('data/StateNames.csv')
+
+top_names = []
+
+# we are essentially going to iterate over the top income states by year, for each state in the year we are then
+# going to look at the most popular names and export a segment of them
+for index, row in income.iterrows():
+    year = row['year']
+    for i in range(1,11):
+        state = row['state_' + str(i)]
+        # select the subset of names for the year and state and get the most popular ones
+
