cc7dd39fdd78f219f39a627dc33800690e10d625
Kyle McCarthy <km@kylemccarthy.io>
2016-04-18 13:46:38 -0500

    analyze the states that have hte lowest median income also

--- income_cleaner.py
+++ income_cleaner.py
@@ -16,6 +16,7 @@ income = income[income.location != 'United States']
 income = income[income.location != 'D.C.']
 
 high_income = []
+low_income = []
 
 for year in range(1984, 2015):
     # sort them by the median income for the year that we are looking at
@@ -23,15 +24,24 @@ for year in range(1984, 2015):
     # get the top 10 states for that year
     highest = sorted_df.head(10)
     highest = highest["location"].tolist()
+    # get the lowest 10 states for that year
+    lowest = sorted_df.tail(10)
+    lowest = lowest["location"].tolist()
     # change the values from state names to state abbreviations to stay consistent with baby names data set
     highest = [us.states.lookup(x).abbr for x in highest]
     highest = [year] + highest
+    lowest = [us.states.lookup(x).abbr for x in lowest]
+    lowest = [year] + lowest
     # add the year + state combo to the list
     high_income.append(highest)
+    low_income.append(lowest)
 
 # generate the headers for the high incomes
-high_income_headers = ["year"] + ["state_" + str(x) for x in range(1, 11)]
+income_headers = ["year"] + ["state_" + str(x) for x in range(1, 11)]
 
 # export the data to a CSV
-high_df = pd.DataFrame(high_income, columns=high_income_headers)
+high_df = pd.DataFrame(high_income, columns=income_headers)
 high_df.to_csv('data/TopMedianIncomeByYear.csv', index=False)
+
+low_df = pd.DataFrame(low_income, columns=income_headers)
+low_df.to_csv('data/LowMedianIncomeByYear.csv', index=False)
--- income_names.py
+++ income_names.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
 income = pd.read_csv('data/TopMedianIncomeByYear.csv')
+income_low = pd.read_csv('data/LowMedianIncomeByYear.csv')
 names = pd.read_csv('data/StateNames.csv')
 
 top_names = pd.DataFrame(columns=names.columns)
+top_low_names = pd.DataFrame(columns=names.columns)
 
 # we are essentially going to iterate over the top income states by year, for each state in the year we are then
 # going to look at the most popular names and export a segment of them
@@ -17,4 +19,17 @@ for index, row in income.iterrows():
         top_names = top_names.append(popular.head(10))
 
 top_names = top_names.drop('Id', axis=1)
-top_names.to_csv('data/TopNamesIncome.csv', index=False)
+top_names.to_csv('data/TopNamesHighIncome.csv', index=False)
+
+# same thing as above but for the low income
+for index, row in income_low.iterrows():
+    year = row['year']
+    for i in range(1,11):
+        state = row['state_' + str(i)]
+        # select the subset of names for the year and state and get the most popular ones
+        popular = names[(names.Year == year) & (names.State == state)]
+        popular = popular.sort_values('Count', ascending=False)
+        top_low_names = top_low_names.append(popular.head(10))
+
+top_low_names = top_low_names.drop('Id', axis=1)
+top_low_names.to_csv('data/TopNamesLowIncome.csv', index=False)
